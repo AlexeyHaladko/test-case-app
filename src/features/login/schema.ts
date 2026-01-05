73bcf77e912e470ae806ecb7cc47e397f53b5f64
Aliaksei.Haladko <aliaksei.haladko@ventionteams.com>
2026-01-05 08:02:29 +0100

    [test-app] fix password error message

--- src/features/login/schema.ts
+++ src/features/login/schema.ts
@@ -3,5 +3,5 @@ import { FieldKeys } from "@/features/login/field-keys.ts";
 
 export const loginSchema = z.object({
   [FieldKeys.username]: z.string().min(1, "Field is required"),
-  [FieldKeys.password]: z.string().min(8, "Field is required"),
+  [FieldKeys.password]: z.string().min(8, "Password too short"),
 })
